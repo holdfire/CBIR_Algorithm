ccea44ada3c6fdb12355646ea40270cdb35556c2
holdfire <1028377876@qq.com>
2019-11-11 12:12:30 +0800

    revise#191111

--- feature/cnn.py
+++ feature/cnn.py
@@ -1,7 +1,9 @@
 import cv2
+from Pillow import Image
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.applications.mobilenet import MobileNet, preprocess_input
+from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.applications.mobilenet import preprocess_input
 
 class CNN:
 	def __init__(self, image_path):
@@ -13,8 +15,15 @@ class CNN:
 
 	def cnn_feature(self):
 		self.model = self.MobileNet(input_shape = (224,224,3),
-									include_top = False, pooling = 'avg')
+									include_top = False,
+									pooling = 'avg')
 		image_pp = self.preprocess_input(self.image)
 		image_pp = np.array(image_pp)[np.newaxis, :]
 		return self.model.predict(image_pp)
+
+
+
+if  __name__ == "__main__":
+	image_path = '../data/query.jpg'
+	image = Image.open(image_path)
 
