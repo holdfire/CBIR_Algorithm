2ca97afe4b2e0e7d382a93c0307f8f060ee0d6c5
holdfire <1028377876@qq.com>
2019-12-03 20:06:12 +0800

    revise#191203

--- preprocess/segmentation.py
+++ preprocess/segmentation.py
@@ -3,61 +3,66 @@ import os
 import numpy as np
 
 
-class SegByContour():
+class SegImg():
     def __init__(self, image_path):
         self.image = cv2.imread(image_path)
 
-
-    def segImage(self, top_k=3, padding=3, threshold=250):
+    def seg_by_contours(self, padding=3, padding_value=[255, 255, 255], threshold=240, maxval=1, type=cv2.THRESH_BINARY):
         '''
-        Attention：广告图片质量非常好，没有噪点。不应该做高斯滤波，做了效果反而不好。可以自己设置滤波器做优化
-        :return: 原图像的top_k个子图的轮廓，其中子图为矩形
+        1. 广告图片质量非常好，没有噪点。不应该做高斯滤波，做了效果反而不好；
+        2.  本算法能很好地切割由黑色和白色组成的边缘
+        :param padding: 在原图（指self.image）的4个边界做填充，目的是检测出原图的4个边界；
+        :param padding_value: 原图边界填充的像素点，默认填充白色像素；
+        :param threshold: 对原图做二值化处理的阈值，默认去白边；如果去黑边则阈值应该设置较大
+        :param maxval: 默认当像素点的值超过上面阈值时，设置为maxval，否则设置为0；
+        :param type: 默认的二值化方法；
+        :return: 原图的最大矩形子图
         '''
-        # 将原图的4个边缘填充白色像素，不做padding的话，原图的边界检测不出来
-        padded_image = cv2.copyMakeBorder(self.image, padding, padding, padding, padding, cv2.BORDER_CONSTANT, value = [255,255,255])
-        # 再处理成灰度图
+        # 对原图依次进行边缘填充，灰度化，二值化处理
+        padded_image = cv2.copyMakeBorder(self.image, padding, padding, padding, padding, cv2.BORDER_CONSTANT, value=padding_value)
         gray_image = cv2.cvtColor(padded_image, cv2.COLOR_BGR2GRAY)
-        # 对灰度图做二值化处理，便于找到轮廓
-        binary_image = cv2.threshold(gray_image, threshold, 255, cv2.THRESH_BINARY)[1].astype(np.uint8)
-        # 寻找图像中块的轮廓，返回一个list，其元素为各个轮廓---->修改为numpy.ndarray类型
-        contours = cv2.findContours(binary_image, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)[1]
-        contours = np.array(contours)
-        # 计算各轮廓所包围块的面积
+        binary_image = cv2.threshold(gray_image, threshold, maxval, type)[1].astype(np.uint8)
+
+        # 检测轮廓-->计算各轮廓包围的面积-->找到面积前2的子图-->排除原图后，返回面积最大的子图在轮廓数组中的索引
+        contours = np.array(cv2.findContours(binary_image, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)[1])
         contours_area = []
         for i in range(len(contours)):
             contours_area.append(cv2.contourArea(contours[i]))
-        # 找到面积最大的前k+1个轮廓的索引，为什么是k+1呢？看下面的注释
-        top_k_index = np.argsort(contours_area)[: :-1][:top_k+1]
-        # 如果子图中包含原图，应该将其去掉
-        if cv2.contourArea(contours[top_k_index[0]]) > self.image.shape[0] * self.image.shape[1]:
-            top_k_index = top_k_index[1:]
+        top2_index = np.argsort(contours_area)[: :-1][:2]
+        # 注意可能没有找到轮廓，这时候只有一个子图即原图，直接返回原图
+        if len(top2_index) < 2:
+            return self.image
+        # 如果检测到的第一大的子图是原图，则取第二大的子图；否则取第一大的子图
+        if cv2.contourArea(contours[top2_index[0]]) > self.image.shape[0] * self.image.shape[1]:
+            max_index = top2_index[1]
         else:
-            top_k_index = top_k_index[:top_k]
-        self.top_k_contours = contours[top_k_index]
-        cv2.drawContours(padded_image, self.top_k_contours, -1, (0,0,255), 2)
-        self.padded_image = np.array(padded_image)
-        return self.padded_image
-        # cv2.imshow("contour_padded_image.png", self.padded_image)
-        # cv2.waitKey(5)
-        # return self.top_k_contours
+            max_index = top2_index[0]
 
+        # 对该面积最大子图轮廓上的点进行排序-->找到左上角点(x1, y1)和右下角点(x2, y2)-->返回包围该子图的一个矩形子图
+        max_counter = np.squeeze(contours[max_index])
+        counter_point_rank = np.sum(max_counter, axis = 1)
+        (x1, y1) = max_counter[np.argmin(counter_point_rank)] - padding + 1
+        (x2, y2) = max_counter[np.argmax(counter_point_rank)] - padding - 1
+        (y_max, x_max) = np.array(self.image.shape[:2]) - 1
+        self.image = self.image[(y1 if y1 >=0 else 0):(y2 if y2<=y_max else y_max), (x1 if x1 >=0 else 0):(x2 if x2<=x_max else x_max)]
+        return self.image
 
-    # def getSubImage(self):
-    #     for i in range(self.top_k_contours)
 
 
 if __name__ == "__main__":
-    # image_path = "../data/test/query3.png"
-    # obj = SegByContour(image_path)
-    # new_image = obj.segImage()
+    # image_path = "../data/test/query.png"
+    # obj = SegImg(image_path)
+    # #obj.seg_by_contours()
+    # new_image = obj.seg_by_contours(padding_value=[0, 0, 0], threshold=50, type=cv2.THRESH_BINARY_INV)
     # os.chdir(os.path.dirname(image_path))
     # cv2.imwrite("processed_image.png", new_image)
 
-    os.chdir("../data/lib2")
+    os.chdir("../data/queries")
     for img in os.listdir(os.getcwd()):
-        obj = SegByContour(img)
-        new_image = obj.segImage()
-        image_path = os.path.join("../lib2_processed",img)
+        obj = SegImg(img)
+        obj.seg_by_contours()
+        new_image = obj.seg_by_contours(padding_value=[0, 0, 0], threshold=10, type=cv2.THRESH_BINARY_INV)
+        image_path = os.path.join("../queries_processed",img)
         cv2.imwrite(image_path, new_image)
 
 
--- preprocess/video2images.py
+++ preprocess/video2images.py
@@ -2,10 +2,11 @@ import cv2
 import os
 
 
-def Video2Images(video_path, frame_interval=3, resize=False, new_size=(128, 128)):
+def video2Images(video_path, frame_interval=3, resize=False, new_size=(128, 128)):
 	# build a directory to store images
-	images_dir = os.path.join(os.path.dirname(video_path), 'images_dir')
-	os.makedirs(images_dir)
+	images_dir = os.path.join(os.path.dirname(os.path.dirname(video_path)), 'img_lib')
+	if not os.path.exists(images_dir):
+		os.makedirs(images_dir)
 
 	video = cv2.VideoCapture(video_path)
 	count = 0
@@ -23,5 +24,23 @@ def Video2Images(video_path, frame_interval=3, resize=False, new_size=(128, 128)
 	return images_dir
 
 
-if __name__ == "__main__":
+
+def video2keyFrames(video_path):
+	'''
+	Extract key frames from a video.
+	:param video_path:
+	:return:
+	'''
 	pass
+
+
+
+
+if __name__ == "__main__":
+	# 将工作目录切换到存放视频的目录，注意：视频广告的名称尽量为合法变量名称字符
+	os.chdir("../data/videos")
+	for video in os.listdir(os.getcwd()):
+		video_path = os.path.join(os.getcwd(), video)
+		video2Images(video_path)
+
+
